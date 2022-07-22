17eb7ed255ac1640456902cdec5987ed95d3df18
AlyLopez02 <alylopez0217@gmail.com>
2022-07-22 18:04:43 -0400

    files set up

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+// TODO: import what returns the whole page put together, for example:
+// import PortfolioContainer from "./components/PortfolioContainer"; //CHANGE THIS
+
+// TODO: define App and input what we imported above
+// const App = () => <PortfolioContainer />; //CHANGE THIS TOO
+
+export default App;
