bc8e3bd270e5f7196cc5f3715729eb459267aa66
alylopez02 <alylopez0217@gmail.com>
2022-07-23 21:40:37 -0400

    further attempt styles

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,16 @@ import React from 'react';
 
 
 export default function Project({ image, linkOne, linkTwo }) {
+  const styles = {
+    container: {
+
+    },
+    overlay: {
+
+    },
+     
+  }
+  
   return (
 
     
