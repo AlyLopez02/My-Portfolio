25463262c0bcc559b60ae215fbbf9f7f85ef5c27
alylopez02 <alylopez0217@gmail.com>
2022-07-23 21:55:23 -0400

    begun filling out Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 
-export default function Project({ image, linkOne, linkTwo }) {
+export default function Project({ name, image, deployedApp, repo }) {
   const styles = {
     container: {
 
@@ -10,14 +10,32 @@ export default function Project({ image, linkOne, linkTwo }) {
     overlay: {
 
     },
-     
+    text: {
+
+    }
   }
-  
+
   return (
 
-    
-    
+    <div className={styles.container}>
+      <img src={image} />
+
+      <div className={styles.overlay}>
+
+        <div className={styles.text}>
+
+          <a href={deployedApp}>{name}</a>
+
+          <br />
+
+          <a href={repo}>GitHub Repository</a>
+
+        </div>
+      </div>
+    </div>
+
   );
 }
 
-//image={image} linkOne={linkOne} linkTwo={linkTwo} ??????
+// maybe use a css file to enable a hover effect? Or maybe use a style sheet from the beginning?
+// all parameters/arguments/whatever-they-are don't seem to be in the styles at all
